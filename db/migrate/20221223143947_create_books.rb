31f3dc8c3d79d6beb3b4ab4ef3d5cf78de128e90
mitragroups <miftahmuhajir@gmail.com>
2022-12-23 21:44:13 +0700

    Migration Books

--- db/migrate/20221223143947_create_books.rb
+++ db/migrate/20221223143947_create_books.rb
@@ -1,10 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.string :title
-      t.integer :page
-      t.integer :price
-      t.text :description
+      t.string :title, :default => 'Belum ada judul', limit: 50
+      t.integer :page, null: false
+      t.integer :price, default: 0
+      t.text :description, default: 'Silahkan isi deskripsi buku tersebut'
 
       t.timestamps
     end

118cfe4beee76f1abebf2fc0ead1390cd653968c
mitragroups <miftahmuhajir@gmail.com>
2023-07-28 11:44:24 +0700

    Add data for Python of Book

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 Book.create(title: 'Buku Go-Lang', price: 75000, page: 75, description: 'Go-Lang adalah pemrograman Backend')
-Book.create(title: 'Buku Go-Lang', price: 75000, page: 75, description: 'Go-Lang adalah pemrograman Backend')
-Book.create(title: 'Buku Go-Lang', price: 75000, page: 75, description: 'Go-Lang adalah pemrograman Backend')
+Book.create(title: 'Buku Java Springboot', price: 450000, page: 450, description: 'Springboot adalah web framework untuk Bahasa Pemrograman Java')
+Book.create(title: 'Buku Python', price: 300000, page: 300, description: 'Cocok untuk yang memperdalam Big Data dan Machine Learning')
 Book.create(title: 'Buku Go-Lang', price: 75000, page: 75, description: 'Go-Lang adalah pemrograman Backend')
 Book.create(title: 'Buku Go-Lang', price: 75000, page: 75, description: 'Go-Lang adalah pemrograman Backend')

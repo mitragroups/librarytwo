25cc64daa294e660802d7da556713feaa22bf611
mitragroups <miftahmuhajir@gmail.com>
2023-07-28 11:21:14 +0700

    Branch Develop

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Book.create(title: 'Buku Go-Lang', price: 75000, page: 75, description: 'Go-Lang adalah pemrograman Backend')
+Book.create(title: 'Buku Go-Lang', price: 75000, page: 75, description: 'Go-Lang adalah pemrograman Backend')
+Book.create(title: 'Buku Go-Lang', price: 75000, page: 75, description: 'Go-Lang adalah pemrograman Backend')
+Book.create(title: 'Buku Go-Lang', price: 75000, page: 75, description: 'Go-Lang adalah pemrograman Backend')
+Book.create(title: 'Buku Go-Lang', price: 75000, page: 75, description: 'Go-Lang adalah pemrograman Backend')

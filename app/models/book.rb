f12b34c315bac76b67e3432e1003e148aa49b0cd
mitragroups <miftahmuhajir@gmail.com>
2023-07-30 05:22:26 +0700

    Add Methode Price Equal More

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
     def self.mahal 
         where('price > 200000')
     end
+    
+    def self.price_equal_more(price)
+        where('price >= ?', price)
+    end
 end

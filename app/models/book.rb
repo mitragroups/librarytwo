9017c7492244ec621cabf413a1ce440427d45e4d
mitragroups <miftahmuhajir@gmail.com>
2023-07-30 17:35:56 +0700

    Add Methode Urutan Harga Buku

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,12 @@ class Book < ApplicationRecord
     def self.mahal 
         where('price > 200000')
     end
-    
+
     def self.price_equal_more(price)
         where('price >= ?', price)
+    end
+
+    def self.urutan_harga 
+        order(price: :asc).pluck(:title, :price, :pages)
     end
 end

e40dec93ed3ff902de4b9d1dc326971ee5e6f086
mitragroups <miftahmuhajir@gmail.com>
2023-08-08 11:01:10 +0700

    Migration Gender Success

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_23_145501) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_08_035448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_23_145501) do
     t.text "address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "gender"
   end
 
   create_table "books", force: :cascade do |t|
